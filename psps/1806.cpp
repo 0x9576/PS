fd425117e8adeee9f36b42a2dfa0ddb18d52c545
gahyu22@gmail.com <gahyu22@gmail.com>
2021-02-10 11:33:12 +0900

    new

--- psps/1806.cpp
+++ psps/1806.cpp
@@ -1,1 +1,12 @@
 #include<iostream>
+using namespace std;
+int e[100000],N,S,l,r,s;
+int main() {
+	cin >> N>>S;
+	for (int i = 0; i < N; i++)
+		scanf("%d",e + i);
+	while (1) {
+		if (S > s)
+			s += e[r++];
+	}
+}
